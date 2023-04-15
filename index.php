d2e34f022f3093ed6813fdc38c568bcb20d36f4d
Amin145875 <93208579+Amin145875@users.noreply.github.com>
2023-04-15 22:23:55 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<<?php 
+session_start();
+
+include("connect.php");
+include("connect.php");
+
+
+$user_data=check_login();
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>My website</title>
+ </head>
+ <body>
+    <a href="logout.php">logout</a>
+    <h1>Thi is my website. thank you .</h1>
+    <br><br>
+    hello, 
+ 
+
+  <body>
+
+ </html>
